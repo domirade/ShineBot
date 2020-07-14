74b339c92baf6cf50ec299e5b36dc130d3ff6b06
zhangshuyang <zhangshuyang@outlook.com>
2020-07-14 16:34:38 +0800

    added some command to get the daily SMs

--- shinebot.py
+++ shinebot.py
@@ -3,6 +3,8 @@ import config
 import logging
 import random
 
+from daily_shadow_mission import daily_async
+
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='shinebot.log', encoding='utf-8', mode='w')
@@ -35,6 +37,27 @@ async def on_message(message):
     # babby's first test command
     if message.content.startswith('%ping'):
         await message.channel.send('PONG!')
+        return
+
+    # daily SM
+    if message.content.startswith('%daily'):
+        today_mission = await daily_async.daily()
+        await message.channel.send(today_mission)
+        return
+    
+    if message.content.startswith('%daily_jp'):
+        today_mission = await daily_async.daily(i18n="JP")
+        await message.channel.send(today_mission)
+        return
+
+    if message.content.startswith('%daily_kr'):
+        today_mission = await daily_async.daily(i18n="KR")
+        await message.channel.send(today_mission)
+        return
+
+    if message.content.startswith('%daily_cn'):
+        today_mission = await daily_async.daily(i18n="ZH_CN")
+        await message.channel.send(today_mission)
         return
 
 client.run(config.token)
