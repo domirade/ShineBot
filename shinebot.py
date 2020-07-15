7a74c2f901a2eb2bfc444f6b31108e0c263f5cf3
Domirade <domimabidomi@gmail.com>
2020-07-15 03:01:38 -0700

    changed token file again (sorry), and some setup for weather functionality.

--- shinebot.py
+++ shinebot.py
@@ -1,15 +1,18 @@
 import config
 import logging
-import shinebot_token
-import math
 import random
 from discord.ext import commands
 from daily_shadow_mission import daily_async
 
+# from daily_shadow_mission import daily_async
+import authtoken
+import weather
+
 # standard logging stuff
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='shinebot.log', encoding='utf-8', mode='w')
+filename = 'shinebottest.log' if config.mode == 'dev' else 'shinebot.log'
+handler = logging.FileHandler(filename=filename, encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
@@ -22,12 +25,14 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or(prefix),
 
 # commands
 
-@bot.command(name='ping')
-async def heartbeat(ctx):
-    """ Check if the bot's alive """
-    response = 'PONG!\n'
+@bot.command(name='ping')    
+async def heartbeat(ctx) -> str:
+    """ Asks the bot for a response.
+    "Hello? Are you still there?" """
+    response = 'pong\n'
     if config.mode == 'dev':
-        response += str(bot.latency)
+        response += 'Latency ' + str(bot.latency) + 'seconds\n'
+        response += f'Called by {authtoken.tester}'
     await ctx.send(response)
         
 @bot.command(name='daily')
@@ -36,13 +41,48 @@ async def DailyShadowMission(ctx, *args):
     response = await daily_async.daily(*args)
     await ctx.send(response)
 
+@bot.command(name='weather')
+async def GetDailyForecast(ctx, area:str='all', day:str='today', time=None, duration:int=2) -> str:
+    """ Gets a weather forecast from Mabinogi World Weather API.
+    
+    Usage: 
+    %weather *<area>*
+    %weather <area> *<day>* 
+    %weather <area> <day> *<duration>*
+    
+    Examples:
+    %weather rano tomorrow
+    %weather taillteann today 18:00 6
+    
+    
+    If run with no arguments, defaults to a 2-hour overview of all regions.
+    
+    Area can be the common name of a map or region, or the numeric regionID used by the game.
+    It defaults to 'all' which also enforces a Duration limit of 2 hours to be polite.
+    
+    Day defaults to 'today' if unspecified which also means 'now' if written in the command.
+    It will otherwise only accept a YYYY-MM-DD date.
+    
+    Time defaults to midnight (from the start of the day) but can be specified in 24:00 or
+    12:00pm (no spaces) notation. Keep in mind that this is in server time and I'm not willing
+    to add a timezone parameter there are already too many. ;)  If you enter a value outside of
+    the 20-minute segments used by the game, it'll be reduced to the nearest one.
+    
+    Duration is the length of the forecast expressed in IRL hours (three 20-minute segments each)
+    It's limited to 36 hours for a single area and 2 hours for all of them.
+    
+    await ctx.send(await weather.forecast.get(area, day, time, duration))
+    """
+    pass
+
 @bot.command()
-async def logout(ctx):
+async def logout(ctx) -> None:
     """ If this is dev, log out and exit """
     if config.mode != 'dev':
         return
     await ctx.send('Logging out now...')
     await bot.logout()
+    bot.close()
     
 @bot.command()
 async def rice(ctx):
@@ -66,8 +106,8 @@ async def rice(ctx):
 @bot.event
 async def on_ready():
     print('Logged in as {0.user}'.format(bot))
-    print('ShineBot version {0.version} build {0.build}'.format(config))
+    print('ShineBot version {0.version}{0.mode} build {0.build}'.format(config))
 
 
 
-bot.run(shinebot_token.token)
+bot.run(authtoken.token)
