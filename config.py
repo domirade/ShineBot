a01f3f85e33d8b37d174258e6b8d0b0e0a4c4633
Domirade <domimabidomi@gmail.com>
2020-08-04 05:39:12 -0700

    Versioning for 0.4

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # should be 'dev' for test mode and 'prod' otherwise
-mode = 'dev'
+mode = 'prod'
 
 # for dev mode, put your username here, in case of multiple test instances running at once
-tester = "Domirade#0413"
-testprefix = '!'
+tester = ""
+testprefix = ''
